539f4e3496ab513acab3faae4e655228a05e3aa7
NicolasBM95 <143222648+NicolasBM95@users.noreply.github.com>
2025-02-06 12:48:00 -0500

    modelo.py

--- informes_cli/modelo.py
+++ informes_cli/modelo.py
@@ -5,6 +5,7 @@ class Entidad(str, Enum):
     ventas = "ventas"
     productos = "productos"
     clientes = "clientes"
+    productos_de_venta = "productos_de_venta"
 
 
 class Agregador(str, Enum):
